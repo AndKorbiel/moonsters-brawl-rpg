974072da3e7d1bb4268ba98289c1372329fc72f3
Andrzej Korbiel <andkorbiel@gmail.com>
2024-01-13 23:05:05 +0100

    refactoring pt.2

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { setStatusCode } from './redux/actions/game';
 import { START_GAME } from './redux/types/game';
+
 import { FightScreenCard, MainMenu, OpponentCard, ShopCard } from './modules/';
 import CharacterCard from './modules/CharacterCard';
-
-import { db } from './firebase-config';
-import { collection } from 'firebase/firestore';
-
 import {
   About,
   HighScore,
@@ -32,77 +29,69 @@ const theme = createTheme({
   },
 });
 
-class App extends React.Component {
-  gameCollectionRef = collection(db, 'games');
+const App = (props) => {
+  const handleGameStatus = (status) => {
+    if (status === 1) props.startGame();
 
-  handleGameStatus = (status) => {
-    if (status === 1) {
-      this.props.startGame();
-    }
-    this.props.setStatusCode(status);
+    props.setStatusCode(status);
   };
 
-  render() {
-    return (
-      <ThemeProvider theme={theme}>
-        <Navbar
-          status={this.props.statusCode}
-          action={this.props.setStatusCode}
-        />
+  return (
+    <ThemeProvider theme={theme}>
+      <Navbar status={props.statusCode} action={props.setStatusCode} />
 
-        <div className="App">
-          <Grid
-            container
-            maxWidth="xl"
-            spacing={2}
-            className="main-cont centered"
-          >
-            {this.props.statusCode === 0 && (
-              <Grid item xs={12}>
-                <MainMenu
-                  action={this.handleGameStatus}
-                  gameStarted={this.props.gameStarted}
-                  options={this.props.menuOptions}
-                />
+      <div className="App">
+        <Grid
+          container
+          maxWidth="xl"
+          spacing={2}
+          className="main-cont centered"
+        >
+          {props.statusCode === 0 && (
+            <Grid item xs={12}>
+              <MainMenu
+                action={handleGameStatus}
+                gameStarted={props.gameStarted}
+                options={props.menuOptions}
+              />
+            </Grid>
+          )}
+
+          {props.statusCode === 1 && (
+            <>
+              <Grid item xs={12} md={4} lg={3}>
+                <CharacterCard />
               </Grid>
-            )}
+              <Grid item xs={12} md={8} lg={9}>
+                <ShopCard />
+              </Grid>
+            </>
+          )}
 
-            {this.props.statusCode === 1 && (
-              <>
-                <Grid item xs={12} md={4} lg={3}>
-                  <CharacterCard />
-                </Grid>
-                <Grid item xs={12} md={8} lg={9}>
-                  <ShopCard />
-                </Grid>
-              </>
-            )}
+          {props.statusCode === 2 && (
+            <>
+              <Grid item xs={12} md={4} lg={3}>
+                <CharacterCard />
+              </Grid>
+              <Grid item xs={12} md={4} lg={6}>
+                <FightScreenCard />
+              </Grid>
+              <Grid item xs={12} md={4} lg={3}>
+                <OpponentCard />
+              </Grid>
+            </>
+          )}
 
-            {this.props.statusCode === 2 && (
-              <>
-                <Grid item xs={12} md={4} lg={3}>
-                  <CharacterCard />
-                </Grid>
-                <Grid item xs={12} md={4} lg={6}>
-                  <FightScreenCard />
-                </Grid>
-                <Grid item xs={12} md={4} lg={3}>
-                  <OpponentCard />
-                </Grid>
-              </>
-            )}
-
-            {this.props.statusCode === 5 && <SaveGame />}
-            {this.props.statusCode === 6 && <LoadGame />}
-            {this.props.statusCode === 7 && <About />}
-            {this.props.statusCode === 8 && <HighScore />}
-            {this.props.statusCode === 9 && <Login />}
-          </Grid>
-        </div>
-      </ThemeProvider>
-    );
-  }
-}
+          {props.statusCode === 5 && <SaveGame />}
+          {props.statusCode === 6 && <LoadGame />}
+          {props.statusCode === 7 && <About />}
+          {props.statusCode === 8 && <HighScore />}
+          {props.statusCode === 9 && <Login />}
+        </Grid>
+      </div>
+    </ThemeProvider>
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
-import { Provider } from 'react-redux'
-import { store } from './redux/store.js'
+import { Provider } from 'react-redux';
+import { store } from './redux/store.js';
 import './styles/index.css';
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>,
-  document.getElementById('root')
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
 );
